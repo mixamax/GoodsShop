205bde466fe57f61502d8d664c3b5e0c3d39edcc
Mixamax <mixamax@inbox.ru>
2024-10-07 14:52:22 +0300

    corrected api changes

--- src/models/authModel.ts
+++ src/models/authModel.ts
@@ -6,7 +6,7 @@ export interface IAuthResponse {
   lastName: string
   gender: string
   image: string
-  token: string
+  accessToken: string
   refreshToken: string
 }
 export interface IBodyRequest {
